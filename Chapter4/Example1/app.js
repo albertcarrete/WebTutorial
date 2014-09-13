6d13e473f52e056dad3eb1560962839ecf75b04e
albertcarrete <albertcarrete@gmail.com>
2014-09-12 23:23:04 -0700

    refactored app.js
    duplicate functions condensed

--- Chapter4/Example1/app.js
+++ Chapter4/Example1/app.js
@@ -1,34 +1,32 @@
 var main = function() {
 	"use strict";
 
-	$(".comment-input button").on("click",function(event){
-		/* Creates a new paragraph element as a jQuery object */
+	var addCommentFromInputBox = function(){
 		var $new_comment;
 
 		if( $(".comment-input input").val() !== ""){
-				$new_comment = $("<p>").text($(".comment-input input").val());
-				$new_comment.hide();
-				$(".comments").append($new_comment);	
-				$new_comment.fadeIn();
-				$(".comment-input input").val("");	// clear input
+			/* Creates a new paragraph element as a jQuery object */
+
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);	
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");	// clear input
 		}
+	}
+
+	$(".comment-input button").on("click",function(event){
+		addCommentFromInputBox();
 	});
 	$(".comment-input input").on("keypress", function(event){
-		var $new_comment;
-
 		/* below is a console log for determining which key
 			applies to which code */
 		// console.log("this is the keyCode" + event.keyCode); 
 		if(event.keyCode === 13){
-			if( $(".comment-input input").val() !== ""){
-				$new_comment = $("<p>").text($(".comment-input input").val());
-				$new_comment.hide();
-				$(".comments").append($new_comment);	
-				$new_comment.fadeIn();
-				$(".comment-input input").val("");	// clear input
-			}		
+			addCommentFromInputBox();	
 		}
 
 	});
 };
+
 $(document).ready(main);
