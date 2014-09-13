1fac9b46d918968a39a4167914a8602705698e34
albertcarrete <albertcarrete@gmail.com>
2014-09-12 19:19:50 -0700

    Added comment input

--- Chapter4/Example1/app.js
+++ Chapter4/Example1/app.js
@@ -2,7 +2,20 @@ var main = function() {
 	"use strict";
 
 	$(".comment-input button").on("click",function(event){
-		$(".comments").append("<p>this is a new comment</p>");
+		/* Creates a new paragraph element as a jQuery object */
+		var $new_comment = $("<p>"),
+		/* Changes the text content of the new paragraph element*/
+		comment_text = $(".comment-input input").val();
+
+		/* We can chain them both together 
+			var $new_comment = $("<p>").text("this is a new comment");
+		*/
+		$new_comment.text(comment_text);
+
+		/* Again,we can just chain all of these lines together
+			var $new_comment = $("<p>").text($(".comment-input input").val());
+		*/
+		$(".comments").append($new_comment);
 	});
 };
 $(document).ready(main);
