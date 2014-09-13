bb2d30cd8769188311b6a7b37b02aef29e15433c
albertcarrete <albertcarrete@gmail.com>
2014-09-12 19:31:56 -0700

    Fixed empty submit error
    Prevents user from sending an empty comment.

--- Chapter4/Example1/app.js
+++ Chapter4/Example1/app.js
@@ -3,19 +3,12 @@ var main = function() {
 
 	$(".comment-input button").on("click",function(event){
 		/* Creates a new paragraph element as a jQuery object */
-		var $new_comment = $("<p>"),
-		/* Changes the text content of the new paragraph element*/
-		comment_text = $(".comment-input input").val();
+		var $new_comment;
 
-		/* We can chain them both together 
-			var $new_comment = $("<p>").text("this is a new comment");
-		*/
-		$new_comment.text(comment_text);
-
-		/* Again,we can just chain all of these lines together
-			var $new_comment = $("<p>").text($(".comment-input input").val());
-		*/
-		$(".comments").append($new_comment);
+		if( $(".comment-input input").val() !== ""){
+			$new_comment = $("<p>").text($(".comment-input input").val());
+			$(".comments").append($new_comment);		
+		}
 	});
 };
 $(document).ready(main);
