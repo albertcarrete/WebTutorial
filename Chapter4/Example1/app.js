e4e4de6c89901cfb954ea3b7f679702d91050a4c
albertcarrete <albertcarrete@gmail.com>
2014-09-12 23:18:45 -0700

    Added keypress submit and fade in
    Comments now fade in / comments can be submitted using the enter key

--- Chapter4/Example1/app.js
+++ Chapter4/Example1/app.js
@@ -6,9 +6,29 @@ var main = function() {
 		var $new_comment;
 
 		if( $(".comment-input input").val() !== ""){
-			$new_comment = $("<p>").text($(".comment-input input").val());
-			$(".comments").append($new_comment);		
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comments").append($new_comment);	
+				$new_comment.fadeIn();
+				$(".comment-input input").val("");	// clear input
 		}
+	});
+	$(".comment-input input").on("keypress", function(event){
+		var $new_comment;
+
+		/* below is a console log for determining which key
+			applies to which code */
+		// console.log("this is the keyCode" + event.keyCode); 
+		if(event.keyCode === 13){
+			if( $(".comment-input input").val() !== ""){
+				$new_comment = $("<p>").text($(".comment-input input").val());
+				$new_comment.hide();
+				$(".comments").append($new_comment);	
+				$new_comment.fadeIn();
+				$(".comment-input input").val("");	// clear input
+			}		
+		}
+
 	});
 };
 $(document).ready(main);
